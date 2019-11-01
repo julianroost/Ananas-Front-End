6c3b4b8a128a473e025e41f703f64b2cf0488baa
Julian Roost <j.roost@student.fontys.nl>
2019-11-01 10:35:43 +0100

    lorem ipsum verbeterd

--- src/App.js
+++ src/App.js
@@ -14,9 +14,16 @@ function App() {
           <div class="col-md-12">
             <h1>Hello, I'm from the Main Page!</h1>
             <p>
-              Aliquip aliquip ullamco deserunt quis nostrud ea amet mollit nisi.
-              Enim nisi sint culpa amet aute Lorem id cillum dolor excepteur
-              aliqua aliquip exercitation. Et elit culpa id est veniam ad.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Nulla fringilla id felis nec congue. Donec euismod, 
+            dolor ac lacinia imperdiet, sem nunc ullamcorper quam, 
+            sit amet interdum ante augue quis nulla. Nullam laoreet in odio in dictum. 
+            Vivamus auctor malesuada risus in mollis. Aliquam feugiat, dui nec imperdiet maximus, 
+            justo nunc congue metus, non pulvinar mauris enim eu diam. 
+            Fusce laoreet fermentum arcu malesuada ultrices. 
+            Nulla consequat aliquet nisl quis tristique. Fusce eleifend sapien nec arcu convallis, 
+            ut cursus est lacinia. Ut sagittis, justo non aliquam accumsan, ipsum quam varius magna, 
+            id auctor odio mi ac odio.
             </p>
             <History />
           </div>
